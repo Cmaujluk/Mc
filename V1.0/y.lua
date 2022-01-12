7108620abb1291c4ab9cd1a3673847aac55f8f9e
Cmaujluk <cmaujluk18@gmail.com>
2022-01-12 20:19:18 +0500

    ыы

--- V1.0/y.lua
+++ V1.0/y.lua
@@ -94,7 +94,6 @@ function ListSearchText(text)
 	if str==nil then List1:insert("nil",123) end
 
 
-
 	for i=1, #items do
 		if string.find(unicode.lower(items[i].label), unicode.lower(str)) then			
 			List1:insert(items[i].label.. " text = "..str..";"..ss,items[i])
@@ -104,25 +103,6 @@ function ListSearchText(text)
 	List1:redraw()
 end
 
-
-[[function ListSearchText(text)
-
-	ss=ss+1
-	local str=text[1]
-	List1:clear()
-
-	if text==nil then List1:insert("nil2",123) 
-	else if str==nil then List1:insert("nil",123) else
-
-
-	for i=1, #items do
-		if string.find(unicode.lower(items[i].label), unicode.lower(str)) then			
-			List1:insert(items[i].label.. " text = "..str..";"..ss,items[i])
-		end
-	end
-	end end
-	List1:redraw()
-end]]
 
 function UpdateCount()
 	local count = tonumber(_count)
