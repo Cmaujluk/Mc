e2af82899bf06cf1358cb0d7606e7feeafcfd1fd
Cmaujluk <cmaujluk18@gmail.com>
2022-01-16 04:26:31 +0500

    1

--- V1.0/magaz.lua
+++ V1.0/magaz.lua
@@ -425,10 +425,10 @@ function AcrivateShopBuyBoughtMenu()
 end
 
 function CreateShopBuyBought()	
-	toShopButton=forms.addForm()
-	toShopButton.W=80
-	toShopButton.H=40
-	toShopButton.color=_mainBackgroundColor
+	_ShopBuyBoughtForm=forms.addForm()
+	_ShopBuyBoughtForm.W=80
+	_ShopBuyBoughtForm.H=40
+	_ShopBuyBoughtForm.color=_mainBackgroundColor
 
 	toShopButton= _ShopBuyBoughtForm:addButton(37,20,"Купить",function() 
 		ActivateShop()		
