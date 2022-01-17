2d261c23016856eb47ce9c463985b879e4e317e7
Cmaujluk <cmaujluk18@gmail.com>
2022-01-17 10:49:37 +0500

    1

--- V1.0/imgInstaller.lua
+++ V1.0/imgInstaller.lua
@@ -2,9 +2,33 @@ local shell = require("shell")
 local fs = require("filesystem")
 local prefix = "https://raw.githubusercontent.com/Cmaujluk/Mc/master/V1.0/mcImgs/"
 
-local imagesCount=16
+local internet = require("internet")
 
-
-for i=1,imagesCount do
-  shell.execute("wget -f "..prefix..i..".png /home/"..i..".png")
+function parseString (inputString)
+  local result = {}
+  for value in string.gmatch(inputString, '".-":".-"') do
+	table.insert(result, (string.gsub(value, '"(.-)":"(.-)"', "%2")))
+  end
+  return result
 end
+
+function RoundToPlaces(value, divisor)
+	return (value * divisor) / divisor
+end
+
+local _itemsBD = {}
+
+getData = internet.request("https://www.toolbexgames.com/mc_getshopdata.php?")
+local result=""
+local product = {}
+for chunk in getData do
+		result = result..chunk
+end
+
+product = parseString(result)
+result = {}
+for i=1,#product/8 do
+	local id = (i-1)*8
+	shell.execute("wget -f "..prefix..product[id+1]..".png /home/"..product[id+1]..".png")
+end
+
