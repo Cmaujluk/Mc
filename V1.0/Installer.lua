5b8590cbe579b5140dbdd29102b6cfc9b23dac2b
Cmaujluk <cmaujluk18@gmail.com>
2022-01-21 17:00:19 +0500

    1

--- V1.0/Installer.lua
+++ V1.0/Installer.lua
@@ -2,9 +2,11 @@ local shell = require("shell")
 local fs = require("filesystem")
 local prefix = "https://raw.githubusercontent.com/Cmaujluk/Mc/master/V1.0/"
 local files = {"/magaz.lua","/forms.lua","/shop.lua","/orechanger.lua","/installer.lua"}
-
+local computer = require("computer") 
 
 for _,v in pairs(files) do
   if not fs.exists(v:match(".*/")) then fs.makeDirectory(v:match(".*/")) end
   shell.execute("wget -f "..prefix..v.." /home/"..v)
 end
+
+computer.shutdown(true)
